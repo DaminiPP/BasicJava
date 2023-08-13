060d29e182d32be1f261c8d6970a39aa54c33a2a
Damini <pdamini120@gmail.com>
2023-08-13 20:09:51 +0530

    Git 2nd Commit

--- src/main/java/BasicJava/ClassesConcepts.java
+++ src/main/java/BasicJava/ClassesConcepts.java
@@ -5,6 +5,10 @@ public class ClassesConcepts {
 	public static void main(String[] args) {
 		
      System.out.println("Deleted Folder Structure");
+     System.out.println("Deleted Folder Structure");
+     System.out.println("Deleted Folder Structure");
+     System.out.println("Deleted Folder Structure");
+     System.out.println("Deleted Folder Structure");
 	}
 
 }
