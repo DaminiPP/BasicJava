0a19d031ebdc6d776fa17b5e4f8303b2fdcb9c31
Damini <pdamini120@gmail.com>
2023-08-13 19:57:33 +0530

    My second git commit

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,6 +1,13 @@
 
 public class TestClass {
 
+	public static void test()
+	{
+		System.out.println("Test");
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		System.out.println("Hell World");
 		System.out.println("Hell World");
---
+++ src/test/java/TestClass2.java
@@ -0,0 +1,14 @@
+
+public class TestClass2 {
+
+	public static void main(String[] args) {
+		System.out.println("Test World");
+		System.out.println("Test World");
+		System.out.println("Test World");
+		System.out.println("Test World");
+
+		System.out.println("Test World");
+		System.out.println("Test World");
+	}
+
+}
