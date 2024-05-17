440290e8647a19b55eff3d498bf243ba9235fcfb
Damini <pdamini120@gmail.com>
2024-05-17 17:40:00 +0530

    Pushing Loops and If Else

---
+++ src/main/java/BasicJava/whileDoWhileLoop.java
@@ -0,0 +1,24 @@
+package BasicJava;
+
+public class whileDoWhileLoop {
+
+	public static void main(String[] args) {
+
+		int i = 1;
+		while (i < 10) {
+			System.out.println("Printing value of i " + i);
+			i++;
+		}
+
+		System.out.println("Do while loop----------");
+
+		int j = 18;
+		do {
+			System.out.println("Print value of j " + j);
+			j++;
+		} 
+		
+		while (j < 10);
+
+	}
+}
